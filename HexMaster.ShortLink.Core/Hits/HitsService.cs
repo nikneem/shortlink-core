692ff5b364133e60e5d0ef0658b67ac00f2f2a81
Eduard Keilholz <e.keilholz@4dotnet.nl>
2020-09-30 16:36:57 +0200

    Update

--- HexMaster.ShortLink.Core/Hits/HitsService.cs
+++ HexMaster.ShortLink.Core/Hits/HitsService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using HexMaster.ShortLink.Core.Contracts;
 using HexMaster.ShortLink.Core.Hits.Contracts;
 
@@ -9,21 +10,18 @@ namespace HexMaster.ShortLink.Core.Hits
         private readonly IHitsRepository _repository;
         private readonly IShortLinksRepository _shortlinksRepository;
 
-        public async Task RegisterHitAsync()
+        public async Task RegisterHitAsync(string shortCode, DateTimeOffset eventDate)
         {
-
-            _repository.CreateNew(shortCode);
-
+            await _repository.CreateNewAsync(eventDate, shortCode);
             await _shortlinksRepository.IncreaseHitsAsync(shortCode);
-
         }
-
         public HitsService(IHitsRepository repository,
             IShortLinksRepository shortlinksRepository)
         {
             _repository = repository;
             _shortlinksRepository = shortlinksRepository;
         }
+
 
     }
 }
